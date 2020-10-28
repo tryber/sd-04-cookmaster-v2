a34a7087457bd587d391ede66aa5ed1749dbdc87
henrique <hbaetaleite@gmail.com>
2020-10-28 15:11:02 -0300

    primeiro commit, sem alteracoes significativas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
