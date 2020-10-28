bd0bead49c559b520d012a53aa21d93f4952bc94
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-10-28 17:37:37 -0300

    check connection

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,9 @@
 const mongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
-const DB_NAME = process.env.DB_NAME || 'Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 const connection = () =>
   mongoClient
