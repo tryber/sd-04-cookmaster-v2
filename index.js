47b45bf450d51a44683e3dea67abd7faf153c9b8
juniomelos <juniomelos@gmail.com>
2020-10-28 14:55:17 -0300

    initial commit project cookmaster v2

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Listening PORT ${PORT}`));
