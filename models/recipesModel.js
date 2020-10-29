ed5b70aec3b70de82912efab9a0438bbe9e2b6db
leandromarten1 <leandromarten1@gmail.com>
2020-10-29 18:03:42 -0300

    CodeClimate 5

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -32,16 +32,13 @@ const createRecipe = async (name, ingredients, preparation, userId) => {
 
 const updateRecipe = async (id, recipe) => {
   const db = await connection();
-  const stmt = await db
-    .collection('recipes')
-    .updateOne({ _id: ObjectId(id) }, { $set: recipe });
+  const stmt = await db.collection('recipes').updateOne({ _id: ObjectId(id) }, { $set: recipe });
   return stmt;
 };
 
 const deleteRecipe = async (id) => {
   const db = await connection();
   await db.collection('recipes').deleteOne({ _id: ObjectId(id) });
-  return;
 };
 
 module.exports = {
@@ -50,5 +47,5 @@ module.exports = {
   createRecipe,
   findByName,
   updateRecipe,
-  deleteRecipe
+  deleteRecipe,
 };
