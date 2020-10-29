935349045f00b00134807b780b5c029ec5f671eb
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-10-29 17:38:22 -0300

    Requisito 7 finalizado

--- services/auth.js
+++ services/auth.js
@@ -6,7 +6,7 @@ const authUser = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
     if (!token) {
-      return res.status(401).json({ message: 'jwt malformed' });
+      return res.status(401).json({ message: 'missing auth token' });
     }
     const decoded = jwt.verify(token, secret);
     const user = await usersModel.findByEmail(decoded.data.email);
