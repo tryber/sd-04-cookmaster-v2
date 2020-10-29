24f22eb38f5a888cd93f20d7b3b7fcabf8ae0750
igorgbr <igorgphotoarte@gmail.com>
2020-10-28 21:08:13 -0300

    primeiro commit

---
+++ models/connection.js
@@ -0,0 +1,17 @@
+const mongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = process.env.DB_NAME || 'StoreManager';
+const connection = () => (
+  mongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+   })
+);
+module.exports = connection;
