48e5d872bb330b2cd2f05467442e3dc7f97653c3
leandromarten1 <leandromarten1@gmail.com>
2020-10-29 23:10:19 -0300

    CodeClimate 6

--- middlewares/uploads.js
+++ middlewares/uploads.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
-const path = require('path');
+// const path = require('path');
 const RecipeModel = require('../models/recipesModel');
 
 const storage = multer.diskStorage({
