edb1d2b0019b9b56110e777fb036bc0ba0c28f62
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-10-30 15:46:14 -0300

    fix/server port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const routes = require('./src/routes');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'uploads')));
