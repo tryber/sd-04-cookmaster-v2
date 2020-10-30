fcd09184ca09453d469a9fd62c2b13634b931f70
Ronald Lima <ronaldazevedolima@gmail.com>
2020-10-30 15:56:47 -0300

    3

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -4,8 +4,7 @@ const addRecipe = async (body) => {
   console.log('model body', body);
   const { name, ingredients, preparation, urlImage, userId } = body;
   const recipe = await connection().then((db) =>
-    db.collection('recipes').insertOne({ name, ingredients, preparation, urlImage, userId }),
-  );
+    db.collection('recipes').insertOne({ name, ingredients, preparation, urlImage, userId }));
   console.log('model recipe.ops', recipe);
   return recipe.ops[0];
 };
