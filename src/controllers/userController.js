eaac5a92908ee95636377d21407666736eb4d03f
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-11-02 22:56:51 -0300

    fix/requirements 1 and 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,10 +3,11 @@ const usersModel = require('../models/userModel');
 const insertUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const { role: userRole } = req.user;
+    const userRole = req.user ? req.user.role : null;
     const insertedUser = await usersModel.insertUser(name, email, password, userRole);
     res.status(201).json({ user: insertedUser });
   } catch (_err) {
+    console.log(_err);
     res.status(500).json({ message: 'Internal error' });
   }
 };
