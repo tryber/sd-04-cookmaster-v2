1ebcba199f1bd859c0d935a3439df3d171f92945
Tiago-ers <trsilva284@gmail.com>
2020-11-05 19:32:37 -0300

    6 ajustes

--- seed.js
+++ seed.js
@@ -1,11 +1,3 @@
-const connection = require('./models/connection');
+const { Db } = require('mongodb');
 
-const userAdmin = { name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' };
-
-const add = async (name, email, password, role) => {
-  const db = await connection();
-  const result = await db.collection('users').insertOne({ name, email, password, role });
-  return result.ops[0];
-};
-
-module.exports = { userAdmin, add };
+Db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
