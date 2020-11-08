87fc9e2b6807cf1908adf8324fb3a355fe6f3a48
Ronald Lima <ronaldazevedolima@gmail.com>
2020-11-08 16:59:44 -0300

    quase

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,18 +1,21 @@
 const userModel = require('../models/usersModel');
 const { schemaAdd } = require('../service/validador');
 
+const addUser = async (data, res) => {
+  const userAdd = await userModel.addUser(data);
+  const { password: _, ...userS } = userAdd;
+  return res.status(201).json({ user: userS });
+};
+
 // validar dados da req e adicionar usuario
 const addUserController = async (req, res) => {
   const { body } = req;
   if (!body.role) body.role = 'user';
-  // const { name: nome, email, password } = body;
   try {
     await schemaAdd.validate(body);
     const checkEmail = await userModel.getUserByEmail(body.email);
     if (!checkEmail) {
-      const userAdd = await userModel.addUser(body);
-      const { password: _, ...userS } = userAdd;
-      return res.status(201).json({ user: userS });
+      return addUser(body, res);
     }
     return res.status(409).json({ message: 'Email already registered' });
   } catch (erro) {
@@ -25,9 +28,7 @@ const addUserAdmin = async (req, res) => {
   if (!body.role) body.role = 'admin';
   const { role } = req.user;
   if (role === 'admin') {
-    const userAdd = await userModel.addUser(body);
-    const { password: _, ...userS } = userAdd;
-    return res.status(201).json({ user: userS });
+    return addUser(body, res);
   }
   return res.status(403).json({ message: 'Only admins can register new admins' });
 };
