9445cf27ad45ee93e7ba7a3b4eb756e9910ef6e8
danilossousa <danilosantossousa@gmail.com>
2020-11-08 13:19:33 -0300

    v0.1.0.5

--- models/connection.js
+++ models/connection.js
@@ -3,10 +3,10 @@ const mongoClient = require('mongodb').MongoClient;
 const DB_NAME = 'Cookmaster';
 
 // Local
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 
 // Online
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 let schema;
 let conn;
