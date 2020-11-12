bbfc3ffa0004527379cad3b2472306f5d65da60a
deboracosilveira <deboracosilveira@gmail.com>
2020-11-12 17:44:30 -0300

    Req 3

---
+++ middlewares/recipesValidation.js
@@ -0,0 +1,35 @@
+const validateToken = require('../auth/validateToken');
+
+const buildResponse = (message) => {
+  const resp = { message };
+  return resp;
+};
+
+const validateAuthenticity = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    const user = validateToken(token);
+    req.user = user;
+
+    next();
+  } catch (_err) {
+    res.status(401).json(buildResponse('jwt malformed'));
+  }
+};
+
+// os campos "name", "ingredients" e "preparation" são obrigatórios
+const validateFields = async (req, res, next) => {
+  const { name, ingredients, preparation } = req.body;
+
+  if (!name || !ingredients || !preparation) {
+    return res.status(400).json(buildResponse('Invalid entries. Try again.'));
+  }
+
+  next();
+};
+
+module.exports = {
+  validateAuthenticity,
+  validateFields,
+};
