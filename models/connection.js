7021ecf500200996328e4010faa56f4b21a73d7a
djonata-dias <diasdjonata@gmail.com>
2020-11-19 19:21:51 -0300

    ajuste no connection para padrão singleton

--- models/connection.js
+++ models/connection.js
@@ -1,21 +1,43 @@
+// const mongoClient = require('mongodb').MongoClient;
+
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// // const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+
+// const DB_NAME = 'Cookmaster';
+
+
+// const connection = () =>
+//   mongoClient
+//     .connect(MONGO_DB_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//     .then((conn) => conn.db(DB_NAME))
+//     .catch((err) => {
+//       console.error(err);
+//       process.exit(1);
+//     });
+
+// module.exports = connection;
 const mongoClient = require('mongodb').MongoClient;
-
-const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-
+let schema = null;
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'Cookmaster';
-
-
-const connection = () =>
-  mongoClient
+async function connection() {
+  if (schema) return Promise.resolve(schema);
+  return mongoClient
     .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then((conn) => conn.db(DB_NAME))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
     .catch((err) => {
       console.error(err);
       process.exit(1);
     });
-
+}
 module.exports = connection;
