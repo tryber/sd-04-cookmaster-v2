72e1f6290b38b7d61423203c78423d2cebd288f9
djonata-dias <diasdjonata@gmail.com>
2020-11-20 17:10:10 -0300

    ajustei o middleware de autenticação

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,26 @@
 const jwt = require('jsonwebtoken');
 
-const { findByEmail } = require('../models/userModel');
-
 const secret = 'NinguemNuncaVaiDescobrirEsteTokenSecreto';
 
-module.exports = async (req, res, next) => {
+const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization;
-
   if (!token) {
-    return res.status(400).json({ error: 'Token não encontrado ou informado' });
+    console.log('missing auth token');
+    return res.status(401).json({ message: 'missing auth token' });
   }
 
   try {
     const decode = jwt.verify(token, secret);
-    const user = await findByEmail(decode.data.email);
+    const user = decode.data;
     if (!user) {
       return res.status(401).json({ message: 'Erro ao procurar usuario do token' });
     }
     req.user = user;
     return next();
   } catch (err) {
-    console.error(err);
-    return res.status(401).json({ message: 'jwt malformed' });
+    // console.error(err);
+    res.status(401).json({ message: 'jwt malformed' });
   }
 };
+
+module.exports = authMiddleware;
