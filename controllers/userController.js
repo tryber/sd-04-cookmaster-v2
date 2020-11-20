6ab489840dbfb424f981068d0649830c0787d79e
djonata-dias <diasdjonata@gmail.com>
2020-11-19 22:21:56 -0300

    Ajustei o controller de cadastrar usuarios para ter apenas um middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const { addUser } = require('../models/userModel');
-const { userDataValidationMiddleware, emailValidationMiddleware } = require('../services/userServices');
+const { userDataValidationMiddleware } = require('../services/userServices');
 
-router.post('/', emailValidationMiddleware, userDataValidationMiddleware, async (req, res) => {
+router.post('/', userDataValidationMiddleware, async (req, res) => {
   const { name, email, password } = req.body;
   const user = await addUser(name, email, password, 'user');
-  console.log(user);
   return res.status(201).json({ user });
 });
 
