0f2a3b627fae0e1217e668e595e1a5f009aba134
djonata-dias <diasdjonata@gmail.com>
2020-11-19 23:42:36 -0300

    criei o controller de recipes

---
+++ controllers/recipesController.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const auth = require('../middlewares/auth');
+
+const { createRecipe } = require('../models/recipesModel');
+
+const { recipeDataValidationMiddleware } = require('../services/recipesService');
+
+const router = express.Router();
+
+router.post('/', auth, recipeDataValidationMiddleware, async (req, res) => {
+  const { name, ingredients, preparation } = req.body;
+  const user = req.user;
+  console.log(user);
+  const recipe = await createRecipe(name, ingredients, preparation, user["_id"]);
+
+  res.status(201).json({ recipe });
+});
+
+module.exports = router;
