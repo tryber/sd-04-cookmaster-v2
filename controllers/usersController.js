c21066936ce329ce56412d82f9bafc95e3baa88c
djonata-dias <diasdjonata@gmail.com>
2020-11-19 22:50:35 -0300

    ajuste de nome do arquivo

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const { addUser } = require('../models/userModel');
+const { createUser } = require('../models/userModel');
 const { userDataValidationMiddleware } = require('../services/userServices');
 
 router.post('/', userDataValidationMiddleware, async (req, res) => {
   const { name, email, password } = req.body;
-  const user = await addUser(name, email, password, 'user');
+  const user = await createUser(name, email, password, 'user');
   return res.status(201).json({ user });
 });
 
