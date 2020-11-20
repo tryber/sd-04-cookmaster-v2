e440775b01c9373de17ea2e6f9b96c618dd6ca51
djonata-dias <diasdjonata@gmail.com>
2020-11-20 17:08:16 -0300

    Deletei código comentado no index

--- index.js
+++ index.js
@@ -14,27 +14,8 @@ app.use('/users', controllers.usersController);
 app.use('/login', controllers.loginController);
 app.use('/recipes', controllers.recipesController);
 
-
-// app.get('/admin', middlewares.auth(), (req, res) =>
-// res.render('admin/home', { user: req.user }));
-
-// app.post('/cadastro', controllers.userController.signup);
-// app.get('/cadastro', controllers.userController.signupForm);
-
-// app.get('/me/edit', middlewares.auth(), controllers.userController.editUserForm);
-// app.post('/me/edit', middlewares.auth(), controllers.userController.editUser);
-
-// app.get('/logout', controllers.userController.logout);
-
-// app.get('/login', controllers.userController.loginForm);
-// app.post('/login', controllers.userController.login);
-
-// app.get('*', (_req, res) => {
-//   res.status(404).json('message: route notFound');
-// });
-
-app.get('/', (request, response) => {
-  response.send();
+app.get('/', (_req, res) => {
+  res.send();
 });
 
 app.listen(3000, () => console.log('rodando'));
