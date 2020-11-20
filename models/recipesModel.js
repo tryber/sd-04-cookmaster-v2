a80791d0cb5378df0c4ae9f9f23c60d5418949d7
djonata-dias <diasdjonata@gmail.com>
2020-11-19 23:43:22 -0300

    ajuste no model de receitas

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -1,8 +1,9 @@
 const connection = require('./connection');
 
-const createRecipe = async (name, ingredients, preparation) =>
-  connection
-    .then((db) => db.collection('recipes').insertOne({ name, ingredients, preparation }))
+const createRecipe = async (name, ingredients, preparation, userId) =>
+  connection()
+    .then((db) => db.collection('recipes').insertOne({ name, ingredients, preparation, userId }))
+    .then((result) => ({ name, ingredients, preparation, userId, _id: result.insertedId }))
     .catch((err) => {
       console.error(err);
       process.exit(1);
