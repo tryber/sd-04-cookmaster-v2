5f3da5ccef824281524af0ffad3f97e87e589f1c
djonata-dias <diasdjonata@gmail.com>
2020-11-20 17:09:55 -0300

    adicionar a rota de atualização e deleção de receita

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const auth = require('../middlewares/auth');
 
-const { createRecipe, getAllRecipes, getRecipeById } = require('../models/recipesModel');
+const {
+  createRecipe,
+  deleteRecipe,
+  updateRecipe,
+  getRecipeById,
+  getAllRecipes,
+} = require('../models/recipesModel');
 
 const {
   recipeDataValidationMiddleware,
@@ -14,7 +20,6 @@ const router = express.Router();
 router.post('/', auth, recipeDataValidationMiddleware, async (req, res) => {
   const { name, ingredients, preparation } = req.body;
   const user = req.user;
-  console.log(user);
   const recipe = await createRecipe(name, ingredients, preparation, user.id);
 
   res.status(201).json({ recipe });
@@ -28,8 +33,26 @@ router.get('/', async (req, res) => {
 router.get('/:id', recipeValidationMiddleware, async (req, res) => {
   const { id } = req.params;
   const recipe = await getRecipeById(id);
-  console.log(recipe);
   return res.status(200).json(recipe);
+});
+
+router.put(
+  '/:id',
+  recipeValidationMiddleware,
+  recipeDataValidationMiddleware,
+  auth,
+  async (req, res) => {
+    const { id } = req.params;
+    const { name, ingredients, preparation } = req.body;
+    const recipe = await updateRecipe(id, name, ingredients, preparation);
+    return res.status(200).json(recipe);
+  },
+);
+
+router.delete('/:id', recipeValidationMiddleware, auth, async (req, res) => {
+  const { id } = req.params;
+  const recipe = await deleteRecipe(id);
+  return res.status(204).json(recipe);
 });
 
 module.exports = router;
