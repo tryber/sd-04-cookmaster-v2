3045f7a4a2bdbb38949a605806715e31d38a868f
djonata-dias <diasdjonata@gmail.com>
2020-11-19 22:22:36 -0300

    retornei o arquivo connection a construção inicial

--- models/connection.js
+++ models/connection.js
@@ -1,46 +1,20 @@
-// const mongoClient = require('mongodb').MongoClient;
-
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-// // const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-
-// const DB_NAME = 'Cookmaster';
-
-// const connection = () =>
-//   mongoClient
-//     .connect(MONGO_DB_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     .then((conn) => conn.db(DB_NAME))
-//     .catch((err) => {
-//       console.error(err);
-//       process.exit(1);
-//     });
-
-// module.exports = connection;
 const mongoClient = require('mongodb').MongoClient;
 
-let schema = null;
-
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
-async function connection() {
-  if (schema) return Promise.resolve(schema);
-  return mongoClient
+const connection = () =>
+  mongoClient
     .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then((conn) => conn.db(DB_NAME))
-    .then((dbSchema) => {
-      schema = dbSchema;
-      return schema;
-    })
     .catch((err) => {
       console.error(err);
       process.exit(1);
     });
-}
+
 module.exports = connection;
