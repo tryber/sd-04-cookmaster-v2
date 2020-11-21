071e2e6abceb067e164082295cf2f5738a21b65f
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-11-21 17:34:07 -0300

    add verifyToken

--- authentication/index.js
+++ authentication/index.js
@@ -19,36 +19,19 @@ const verifyToken = (req, res, next) => {
   return res.status(401).json({ message: 'jwt malformed' });
 };
 
-// const validateToken = (required = true) => (req, _res, next) => {
-//   if (!required) {
-//     req.required = false;
-//     return next();
-//   }
-//   try {
-//     const token = req.headers.authorization;
-//     req.required = required;
-//     const user = jwt.verify(token, secret);
-//     req.user = user;
-//     return next();
-//   } catch (_err) {
-//     return next();
-//   }
-// };
-
-const validateToken = (required = true) => (req, res, next) => {
+const validateToken = (required = true) => (req, _res, next) => {
   if (!required) {
     req.required = false;
     return next();
   }
-  req.required = true;
-  const token = req.headers.authorization;
-  if (!token) return res.status(401).json({ message: 'missing auth token' });
   try {
+    const token = req.headers.authorization;
+    req.required = required;
     const user = jwt.verify(token, secret);
     req.user = user;
     return next();
   } catch (_err) {
-    return res.status(401).json({ message: 'jwt malformed' });
+    return next();
   }
 };
 
