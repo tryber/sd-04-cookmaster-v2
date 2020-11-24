38147b7e0ad6bf905cf2331de191ab0202837eb2
Giorgia <giorgialeoniac@gmail.com>
2020-11-24 11:38:20 -0300

    Requisitos 1 e 2 feitos

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const userModel = require('../models/userModel');
+const tokenMid = require('../middlewares/token');
 
 // Login
 router.post('/',
   async (req, res) => {
     const { email, password } = req.body;
     const user = await userModel.findByEmail(email);
+    console.log(user);
 
     if (!email || !password) {
       return res.status(401).json({ message: 'All fields must be filled' });
@@ -15,7 +17,10 @@ router.post('/',
     if (!user || user.password !== password) {
       return res.status(401).json({ message: 'Incorrect username or password' });
     }
-    // O login com sucesso depende do jwt
+
+    const { password: _, ...data } = user;
+    const token = tokenMid.createToken(data);
+    return res.status(200).json({ token });
   });
 
 module.exports = router;
