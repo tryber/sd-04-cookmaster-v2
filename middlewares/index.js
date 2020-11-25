6637d6dafcfd21a5dfa1c3a84c88d04058089dd4
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-11-25 13:15:44 -0300

    Correção

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,9 @@
 const errorHandler = require('./errorHandler');
-const auth = require('./auth');
 const usersValidation = require('./usersValidation');
 const loginValidation = require('./loginValidation');
 
 module.exports = {
   errorHandler,
-  auth,
   usersValidation,
   loginValidation,
 };
