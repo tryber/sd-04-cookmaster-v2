3ca45f9f5eea9031440ad1446994f1aae8564b87
Luma Arruda <luma.oarruda@gmail.com>
2020-11-28 23:56:33 -0300

    rotas

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const path = require('path');
+
+const { userController, loginController, recipesController } = require('./controllers');
 
 const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.use('/users', userController);
+app.use('/login', loginController);
+app.use('/recipes', recipesController);
+app.use('/images', express.static(path.join(__dirname, '/images')));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
